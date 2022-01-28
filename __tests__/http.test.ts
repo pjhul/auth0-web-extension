62fc2b32bbe65ccee6c048379cac88ba72793584
Paul Hultgren <pjhultgren5@gmail.com>
2022-01-28 15:20:57 -0500

    remove unfetch polyfill and fix tests

--- __tests__/http.test.ts
+++ __tests__/http.test.ts
@@ -1,20 +1,12 @@
-import fetch from 'unfetch';
-
 import { MfaRequiredError } from '../src/errors';
 import { fetchWithTimeout, fetchJSON } from '../src/http';
 
-jest.mock('unfetch');
-
-const mockUnfetch = <jest.Mock>fetch;
-
 describe('fetchWithTimeout', () => {
   it('clears timeout when successful', async () => {
-    mockUnfetch.mockImplementation(
-      jest.fn().mockResolvedValue({
-        ok: true,
-        json: () => Promise.resolve(),
-      })
-    );
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: true,
+      json: () => Promise.resolve(),
+    });
 
     jest.spyOn(window, 'clearTimeout');
     await fetchWithTimeout('https://test.com/', {}, undefined);
@@ -26,12 +18,10 @@ describe('fetchWithTimeout', () => {
 
 describe('fetchJSON', () => {
   it('throws MfaRequiredError when mfa_required is returned', async () => {
-    mockUnfetch.mockImplementation(
-      jest.fn().mockResolvedValue({
-        ok: false,
-        json: () => Promise.resolve({ error: 'mfa_required' }),
-      })
-    );
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: false,
+      json: () => Promise.resolve({ error: 'mfa_required' }),
+    });
 
     await expect(
       fetchJSON('https://test.com/', {}, undefined)
@@ -39,13 +29,10 @@ describe('fetchJSON', () => {
   });
 
   it('reads the mfa_token when mfa_required is returned', async () => {
-    mockUnfetch.mockImplementation(
-      jest.fn().mockResolvedValue({
-        ok: false,
-        json: () =>
-          Promise.resolve({ error: 'mfa_required', mfa_token: '1234' }),
-      })
-    );
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: false,
+      json: () => Promise.resolve({ error: 'mfa_required', mfa_token: '1234' }),
+    });
 
     await expect(
       fetchJSON('https://test.com/', {}, undefined)
